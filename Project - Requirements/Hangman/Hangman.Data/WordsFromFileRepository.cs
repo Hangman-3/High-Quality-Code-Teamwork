5ed8373de7f877d1b94cc35281e703f86ddb5229
Martin Nikolov <flextry@gmail.com>
2014-07-13 16:14:25 +0300

    add assebly/executable files

---
+++ Project - Requirements/Hangman/Hangman.Data/WordsFromFileRepository.cs
@@ -0,0 +1,51 @@
+﻿namespace Hangman.Data
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using System.IO;
+    using System.Linq;
+    using Hangman.Common.Interfaces;
+
+    public class WordsFromFileRepository : IWordsRepository
+    {
+        private const string WordsFilePath = "../../words.txt";
+
+        private IList<string> words;
+
+        public WordsFromFileRepository()
+        {
+            this.words = this.ReadWordsFromFile(out this.words);
+        }
+
+        public IReadOnlyCollection<string> Words
+        {
+            get { return new ReadOnlyCollection<string>(this.words); }
+        }
+
+        private IList<string> ReadWordsFromFile(out IList<string> wordsCollection)
+        {
+            wordsCollection = new List<string>();
+
+            if (!File.Exists(WordsFilePath))
+            {
+                throw new FileNotFoundException("Words file does not exists.");
+            }
+
+            using (var reader = new StreamReader(WordsFilePath))
+            {
+                while (!reader.EndOfStream)
+                {
+                    string word = reader.ReadLine();
+
+                    if (!string.IsNullOrEmpty(word))
+                    {
+                        wordsCollection.Add(word);
+                    }
+                }
+            }
+
+            return wordsCollection;
+        }
+    }
+}
