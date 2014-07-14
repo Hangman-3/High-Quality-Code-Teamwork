44e16b67b0946c329d4d73622e41f9b44d156d6e
adzhazhev <alex.dzhazhev@gmail.com>
2014-07-14 19:21:35 +0300

    Hangman.Console added class NinjectoThreadSafe

---
+++ Project - Requirements/Hangman/Hangman.Console/NinjectoThreadSafe.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman.Console
+{
+    public sealed class NinjectoThreadSafe
+    {
+        private static volatile NinjectoThreadSafe instance; // volatile modifier is used to show that the variable will be accessed by multiple threads concurrently.
+
+        private static object syncLock = new object();
+
+        private NinjectoThreadSafe() { }
+
+        public static NinjectoThreadSafe Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (syncLock)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new NinjectoThreadSafe();
+                        }
+                    }
+                }
+
+                return instance;
+            }
+        }
+    }
+}
