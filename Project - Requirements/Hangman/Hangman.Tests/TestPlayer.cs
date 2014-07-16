e72e79be9059160207cd68c459e3ad2fa9170e8a
mTonkov <mtonkov@yahoo.com>
2014-07-17 01:50:00 +0300

    Added tests for Player.Clone() and Player's MistakesCount setter-s

---
+++ Project - Requirements/Hangman/Hangman.Tests/TestPlayer.cs
@@ -0,0 +1,32 @@
+﻿namespace Hangman.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using Hangman.Common.Interfaces;
+    using Hangman.Models;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class TestPlayer
+    {
+        [TestMethod]
+        public void TestClonePlayer()
+        {
+            var player = new Player() { Name="Gosho", MistakesCount = 2};
+            var score = new Scoreboard();
+            score.AddPlayer(player.Clone() as Player);
+
+            var players = new List<IPlayer>(score.Players);
+
+            Assert.AreEqual(player.Name, players[0].Name, "Name of the cloned person is not the same");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestForWrongInitializationOfPlayerName()
+        {
+            var player = new Player() { Name = "Gosho", MistakesCount = -2 };
+        }
+
+    }
+}
