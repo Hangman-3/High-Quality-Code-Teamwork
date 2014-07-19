b6b219388fb967f6fbf5b698b4d81beed8a6a5cd
adzhazhev <alex.dzhazhev@gmail.com>
2014-07-19 12:24:11 +0300

    Added documentation for IWord interface

--- Project - Requirements/Hangman/Hangman.Common/Interfaces/IWord.cs
+++ Project - Requirements/Hangman/Hangman.Common/Interfaces/IWord.cs
@@ -1,11 +1,23 @@
+// <copyright file="IWord.cs" company="Telerik Academy">
+//   Copyright (c) Telerik Academy. All rights reserved.
+// </copyright>
 namespace Hangman.Common.Interfaces
 {
     using System.Text;
 
+    /// <summary>
+    /// Interface that word classes must implement
+    /// </summary>
     public interface IWord
     {
+        /// <summary>
+        /// Gets or sets the value of the original word
+        /// </summary>
         StringBuilder Original { get; set; }
 
+        /// <summary>
+        /// Gets or sets the value of the player's guessed up until now word
+        /// </summary>
         StringBuilder Secret { get; set; }
     }
 }
