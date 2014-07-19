c3938f41ad0827779b1a86d08849de58e3170d61
adzhazhev <alex.dzhazhev@gmail.com>
2014-07-19 12:21:44 +0300

    Added documentation for IScoreboard interface

--- Project - Requirements/Hangman/Hangman.Common/Interfaces/IScoreboard.cs
+++ Project - Requirements/Hangman/Hangman.Common/Interfaces/IScoreboard.cs
@@ -1,16 +1,26 @@
-﻿namespace Hangman.Common.Interfaces
+﻿// <copyright file="IScoreboard.cs" company="Telerik Academy">
+//   Copyright (c) Telerik Academy. All rights reserved.
+// </copyright>
+namespace Hangman.Common.Interfaces
 {
     using System;
     using System.Collections.Generic;
     using System.Linq;
 
     /// <summary>
-    /// 
+    /// Interface that scoreboard classes must implement
     /// </summary>
     public interface IScoreboard
     {
+        /// <summary>
+        /// Gets a collection containing all player objects
+        /// </summary>
         IReadOnlyCollection<IPlayer> Players { get; }
 
+        /// <summary>
+        /// Adds an object implementing IPlayer to the scoreboard
+        /// </summary>
+        /// <param name="player">Object containing player information</param>
         void AddPlayer(IPlayer player);
     }
 }
