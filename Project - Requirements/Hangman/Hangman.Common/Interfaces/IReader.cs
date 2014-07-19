6ca48cb3b9758f0e239d80348c5778d12e4b331a
adzhazhev <alex.dzhazhev@gmail.com>
2014-07-19 12:16:19 +0300

    Added IReader interface documentation

--- Project - Requirements/Hangman/Hangman.Common/Interfaces/IReader.cs
+++ Project - Requirements/Hangman/Hangman.Common/Interfaces/IReader.cs
@@ -1,12 +1,19 @@
-﻿namespace Hangman.Common.Interfaces
+﻿// <copyright file="IReader.cs" company="Telerik Academy">
+//   Copyright (c) Telerik Academy. All rights reserved.
+// </copyright>
+namespace Hangman.Common.Interfaces
 {
     using System;
 
     /// <summary>
-    /// 
+    /// Interface that reader classes must implement
     /// </summary>
     public interface IReader
     {
+        /// <summary>
+        /// Reads console input
+        /// </summary>
+        /// <returns>Returns console input as a string</returns>  
         string Read();
     }
 }
