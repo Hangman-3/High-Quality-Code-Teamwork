34510503c9582066230f4ea796ac202a761b01bf
adzhazhev <alex.dzhazhev@gmail.com>
2014-07-19 12:07:59 +0300

    Added documentation for the IPlayer interface

--- Project - Requirements/Hangman/Hangman.Common/Interfaces/IPlayer.cs
+++ Project - Requirements/Hangman/Hangman.Common/Interfaces/IPlayer.cs
@@ -1,15 +1,24 @@
-﻿namespace Hangman.Common.Interfaces
+﻿// <copyright file="IPlayer.cs" company="Telerik Academy">
+//   Copyright (c) Telerik Academy. All rights reserved.
+// </copyright>
+namespace Hangman.Common.Interfaces
 {
     using System;
     using System.Linq;
 
     /// <summary>
-    /// 
+    /// Interface that player classes must implement
     /// </summary>
     public interface IPlayer : ICloneable
     {
+        /// <summary>
+        /// Gets or sets string property containing player name
+        /// </summary>
         string Name { get; set; }
 
+        /// <summary>
+        /// Gets or sets 32bit integer property for counting the player mistakes
+        /// </summary>
         int MistakesCount { get; set; }
     }
 }
