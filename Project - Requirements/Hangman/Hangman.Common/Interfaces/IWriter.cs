655413580c38b9756eeb5c0ec58c9c5f294c89ce
adzhazhev <alex.dzhazhev@gmail.com>
2014-07-19 13:24:01 +0300

    Added IWriter interface documentation

--- Project - Requirements/Hangman/Hangman.Common/Interfaces/IWriter.cs
+++ Project - Requirements/Hangman/Hangman.Common/Interfaces/IWriter.cs
@@ -1,17 +1,33 @@
-﻿namespace Hangman.Common.Interfaces
+﻿// <copyright file="IWriter.cs" company="Telerik Academy">
+//   Copyright (c) Telerik Academy. All rights reserved.
+// </copyright>
+namespace Hangman.Common.Interfaces
 {
     using System;
     using System.Text;
 
     /// <summary>
-    /// 
+    /// Interface that writer classes must implement
     /// </summary>
     public interface IWriter
     {
+        /// <summary>
+        /// Displays a message
+        /// </summary>
+        /// <param name="message">String containing a message</param>
+        /// <param name="placeHolders">Object containing placeholders</param>
         void ShowMessage(string message, params object[] placeHolders);
 
+        /// <summary>
+        /// Displays the secret word
+        /// </summary>
+        /// <param name="secretWord">The word that has to be displayed</param>
         void ShowSecretWord(StringBuilder secretWord);
 
+        /// <summary>
+        /// Displays the scoreboard
+        /// </summary>
+        /// <param name="scoreboard">Scoreboard to be displayed</param>
         void ShowScoreboard(IScoreboard scoreboard);
     }
 }
