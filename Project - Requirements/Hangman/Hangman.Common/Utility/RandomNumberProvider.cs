7fecd28318ba3702d8a2aa216ef4c18c6dbf99f8
mTonkov <mtonkov@yahoo.com>
2014-07-23 17:30:46 +0300

    "Singleton" implementation for the RandomGenerator

---
+++ Project - Requirements/Hangman/Hangman.Common/Utility/RandomNumberProvider.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman.Common.Utility
+{
+    public sealed class RandomNumberProvider
+    {
+        private static readonly Random instance = new Random();
+
+        public static Random NewRandomizer
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+    }
+}
