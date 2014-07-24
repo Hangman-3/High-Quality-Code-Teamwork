abb0289929c56e03a88aecab58204c49cccebe97
Martin Nikolov <flextry@gmail.com>
2014-07-24 14:56:03 +0300

    Solution diagram updated Add Command.cs Add CommandFactory.cs

---
+++ Project - Requirements/Hangman/Hangman.Models/Command.cs
@@ -0,0 +1,50 @@
+﻿namespace Hangman.Models
+{
+    using System;
+    using System.Linq;
+    using Hangman.Common.Enums;
+    using Hangman.Common.Interfaces;
+
+    /// <summary>
+    /// Keeps an instance of Commands based on user input
+    /// </summary>
+    public class Command : ICommand
+    {
+        private string arguments;
+
+        /// <summary>
+        ///  Initializes a new instance of the <see cref="Command" /> class.
+        /// </summary>
+        /// <param arguments="arguments">Command's arguments based on user input</param>
+        public Command(string arguments)
+        {
+            this.Arguments = arguments;
+        }
+
+        /// <summary>
+        /// Keeps the command's arguments
+        /// </summary>
+        public string Arguments
+        {
+            get
+            {
+                return this.arguments;
+            }
+
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException("Command arguments cannot be null or empty.");
+                }
+
+                arguments = value;
+            }
+        }
+
+        /// <summary>
+        /// Keeps the command type (enumeration)
+        /// </summary>
+        public CommandType Type { get; set; }
+    }
+}
---
+++ Project - Requirements/Hangman/Hangman.Models/CommandFactory.cs
@@ -0,0 +1,58 @@
+﻿namespace Hangman.Models
+{
+    using System;
+    using System.Linq;
+    using Hangman.Common.Enums;
+    using Hangman.Common.Interfaces;
+
+    /// <summary>
+    /// Gets the type of a Command
+    /// </summary>
+    public static class CommandFactory
+    {
+        /// <summary>
+        /// Gets the Command's type based on its name
+        /// </summary>
+        /// <param arguments="commandArgs">Command's arguments</param>
+        /// <returns>Command type as an enumeration</returns>
+        public static ICommand ParseCommand(string commandArgs)
+        {
+            var command = new Command(commandArgs);
+
+            switch (commandArgs.ToLower())
+            {
+                case "top":
+                    {
+                        command.Type = CommandType.Top;
+                        break;
+                    }
+
+                case "restart":
+                    {
+                        command.Type = CommandType.Restart;
+                        break;
+                    }
+
+                case "help":
+                    {
+                        command.Type = CommandType.Help;
+                        break;
+                    }
+
+                case "exit":
+                    {
+                        command.Type = CommandType.Exit;
+                        break;
+                    }
+
+                default:
+                    {
+                        command.Type = CommandType.Default;
+                        break;
+                    }
+            }
+
+            return command;
+        }
+    }
+}
